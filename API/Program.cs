f9b79b7a8983eda0a95e3d9e0ebd5add9b8c3f12
Katerina Popova <it.katerinapopova@gmail.com>
2025-11-09 22:22:57 +0100

    added react skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
